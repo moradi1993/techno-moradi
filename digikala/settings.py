75a24e256c3bcb2d4b18c5befe1e6b5b157d8cfc
moradi1993 <mohammad.moradi6030@gmail.com>
2025-05-03 21:00:07 +0330

    Update settings.py

--- digikala/settings.py
+++ digikala/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 #STATIC_URL = 'static/'
-STATICFILES_URLS = ['/static/']
+STATICFILES_URLS = ['static/']
 MEDIA_URL ='media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
